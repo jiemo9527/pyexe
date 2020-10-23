c0ab0b49289182b3b746e3fcb140d78ff583732d
JIEMO <2569995914@qq.com>
2020-10-23 12:11:02 +0800

    init

--- ffmpeg_download_m3u8.py
+++ ffmpeg_download_m3u8.py
@@ -9,7 +9,7 @@ import subprocess
 
 def ffmpeg_download_m3u8():
     url = input("输入m3u8链接：")
-    subprocess.Popen('ffmpeg -threads 32 -i %s -c copy -movflags +faststart out.MP4' % (url), shell=True)
+    subprocess.Popen('ffmpeg -threads 32 -i %s -c copy -movflags +faststart out.mp4' % (url), shell=True)
 
 
 if __name__ == '__main__':
