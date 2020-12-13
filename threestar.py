d75dffb8eff2efb2eb5bafa8d4bf093b70604557
JIEMO <2569995914@qq.com>
2020-12-13 16:04:55 +0800

    update

--- threestar.py
+++ threestar.py
@@ -21,7 +21,7 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9
            "Accept-Language": "en-us",
            "Connection": "keep-alive",
            "Accept-Charset": "GB2312,utf-8;q=0.7,*;q=0.7",
-           "Cookie": ""}
+           "Cookie": "PHPSESSID=85brk17vm3vl81eav5jj3rghc5; the_cookie=1; shuidiV=1; userAccount=17761715530; userHit=true; cGlobal[authID]=5258215; cGlobal[authTime]=1607831077; cGlobal[times]=1; ckurl.lezhuan.com=%2Ffast%2F; cGlobal[last]=1607837630"}
 for i in range(1, int(xxx)):
     purl.append("https://www.lezhuan.com/fast/?p=" + str(i))
 for x in range(len(purl)):
@@ -73,21 +73,24 @@ for flag in range(0, len(sta) - 6):
         flag + 6] == sta[flag + 7] == sta[flag + 8] == sta[flag + 9] \
             and sta[flag + 9] != sta[flag + 10] and sta[flag] != sta[flag - 1]:
         total.append(sta[flag] + "*10")
-    if sta[flag] == sta[flag + 1] == sta[flag + 2] == sta[flag + 4] == sta[flag + 5] == "中" and\
-            sta[flag] != sta[flag - 1] and sta[flag + 3] == "边":
-        total.append("边全败")
-    if sta[flag] == sta[flag + 1] == sta[flag + 2] == sta[flag + 4] == sta[flag + 5] == "边" and\
-            sta[flag] != sta[flag - 1] and sta[flag + 3] == "中":
-        total.append("中全败")
+    if sta[flag] == sta[flag + 1] == sta[flag + 2] == sta[flag + 4] == "边" and \
+            sta[flag] != sta[flag - 1] and sta[flag + 3] == sta[flag - 1]:
+        total.append("三切全败")
+    if sta[flag] == sta[flag + 1] ==sta[flag + 3] == "边" and \
+            sta[flag] != sta[flag - 1] and sta[flag + 2] == sta[flag + 4]==sta[flag-1]:
+        total.append("双切全败")
+    if sta[flag] == sta[flag + 2] ==sta[flag + 4] == "边" and \
+            sta[flag] != sta[flag - 1] and sta[flag + 1] == sta[flag + 3]==sta[flag-1]:
+        total.append("单切全败")
 
 print("统计样本次数  ：  " + str(len(total)))
 A = Counter(total).most_common()
 print("详细情况  ：  \n" + str(A) + "\n概率转化：")
 for i in range(len(A)):
-    if A[i][0].find("全败")==-1:
+    if A[i][0].find("全败") == -1:
         yu = int(A[i][0].split("*")[1]) - 1
-        print(A[i][0] + "  :  " + str(round((int(A[i][1]) / int(len(total)-yu) * 100), 2)) + "%")
+        print(A[i][0] + "  :  " + str(round((int(A[i][1]) / int(len(total) - yu) * 100), 2)) + "%")
     else:
-        print(A[i][0] + "  :  " + str(round((int(A[i][1]) / int(len(total)-5) * 100), 2)) + "%")
+        print(A[i][0] + "  :  " + str(round((int(A[i][1]) / int(len(total) - 4) * 100), 2)) + "%")
 
 os.system('pause')
