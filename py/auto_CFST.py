34f87622bb38b110792956b8fdf677d124d89812
JIEMO <2569995914@qq.com>
2021-07-01 09:43:52 +0800

    update auto_CFST

--- py/auto_CFST.py
+++ py/auto_CFST.py
@@ -8,7 +8,7 @@ from logging import info
 
 
 
-host_path="C:\WINDOWS\system32\drivers\etc\HOSTS"
+
 # 打开excel获取
 #pandas打包太大，弃
 # cfst_ip=pandas.read_csv("D:\\NETtool\GFW\\CloudflareST_windows_amd64\\result.csv").iloc[-1]["IP 地址"]
@@ -16,24 +16,35 @@ excel=open("D:\\NETtool\GFW\\CloudflareST_windows_amd64\\result.csv","r",encodin
 opened_file = excel.readlines()
 cfst_ip=opened_file[1].split(',')[0]
 print("获取优选ip("+cfst_ip+")成功.\n")
-change_dns=cfst_ip+"		st.rn.cdnets.cyou"
-hosts= open(host_path, 'rb+')
-line = hosts.readline()
+
+
+change_dns=cfst_ip+"		"
+
+def lastline():
 # 删除最后一行
-pos = [0, hosts.tell()]
-while line != b'':
+    host_path="C:\WINDOWS\system32\drivers\etc\HOSTS"
+    hosts= open(host_path, 'rb+')
     line = hosts.readline()
-    temp = hosts.tell()
-    if temp != pos[1]:
-        pos[0] = pos[1]
-        pos[1] = temp
-hosts.seek(pos[0], 0)
-hosts.truncate()
-hosts.close()
+    pos = [0, hosts.tell()]
+    while line != b'':
+        line = hosts.readline()
+        temp = hosts.tell()
+        if temp != pos[1]:
+            pos[0] = pos[1]
+            pos[1] = temp
+    hosts.seek(pos[0], 0)
+    hosts.truncate()
+    hosts.close()
+lastline()
+lastline()
 print("已清除旧的dns记录.\n")
+def addline(change_dns):
 #插入新的dns记录
-hosts=open(host_path,'a')
-hosts.write(change_dns)
-hosts.close()
-print("新增dns记录成功.\n")
+    host_path="C:\WINDOWS\system32\drivers\etc\HOSTS"
+    hosts=open(host_path,'a')
+    hosts.write(change_dns)
+    hosts.close()
+    print("新增dns记录成功.\n")
+addline(change_dns+"rn.cdnets.cyou\n")
+addline(change_dns+"ww.cdnets.cyou")
 os.system('pause')
